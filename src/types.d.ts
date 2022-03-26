40fbf8914ce46b38d2a7bb594c55c23e7a6179b2
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-03-26 19:12:13 +0200

    update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,52 +1,51 @@
 export type Variant =
   | "success"
   | "danger"
-  | "disabled"
   | "error"
   | "success-outlined"
   | "danger-outlined"
   | "error-outlined"
   | "default-outlined";
 
-  export type CommonProps = {
-    key?: string|number;
-    className?: string;
-    backgroundColor?: string;
-    color?: string;
-    border?: string    
-    borderRadius?: string;
-    onClick?: ()=> void;
-  }
-  export interface IButton {
-    key?: string|number;
-    children?: React.ReactNode | string | number;
-    style?: CSSProperties;
-    variant?: Variant;
-    className?: string;
-    outlined?: boolean;
-    backgroundColor?: string;
-    fullWidth?: string;
-    color?: string;
-    borderRadius?: string;
-    padding?: string;
-    fontSize?: string;
-    rounded?: boolean;
-    border?: string;
-    icon?: ReactElement;
-    iconLeft?: ReactElement;
-    disabled?: boolean;
-    onClick?: ()=> void;
-  }
-  
-  export interface IBadge {
-    key?: string|number;
-    className?: string;
-    children: ReactNode;
-    color?: string;
-    background?: string;
-    label?: string | number;
-    borderRadius?: string;  
-    border?: string    
-    onClick?: ()=> void;
-    position?: top | bottom | "top-left" | "bottom-left";
-    }
+export type CommonProps = {
+  key?: string | number;
+  className?: string;
+  backgroundColor?: string;
+  color?: string;
+  border?: string;
+  borderRadius?: string;
+  onClick?: () => void;
+};
+export interface IButton {
+  key?: string | number;
+  children?: React.ReactNode | string | number;
+  style?: CSSProperties;
+  variant?: Variant;
+  className?: string;
+  backgroundColor?: string;
+  fullWidth?: boolean;
+  color?: string;
+  borderRadius?: string;
+  padding?: string;
+  fontSize?: string;
+  rounded?: boolean;
+  border?: string;
+  icon?: ReactElement;
+  iconLeft?: ReactElement;
+  disabled?: boolean;
+  onClick?: () => void;
+  justify?: "center" | "flex-start" | "flex-end" | "stretch";
+}
+
+export interface IBadge {
+  key?: string | number;
+  className?: string;
+  children: ReactNode;
+  color?: string;
+  background?: string;
+  label?: string | number;
+  borderRadius?: string;
+  border?: string;
+  onClick?: () => void;
+  position?: top | bottom | "top-left" | "bottom-left";
+}
