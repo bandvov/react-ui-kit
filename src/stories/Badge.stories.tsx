9a840d9097d435be75ac1ea27d4263d755490a8c
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-03-27 14:22:12 +0300

    add badge documentation

---
+++ src/stories/Badge.stories.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Badge from "../components/Badge";
+import Button from "../components/Button/Button";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Badge",
+  component: Badge,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    position: {
+      options: [
+      "top","bottom",
+      ],
+      control: { type: "radio" },
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
+
+export const BadgeTemplate = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+BadgeTemplate.args = {
+  label:"",
+  children: <Button rounded >&copy;</Button>,
+};
