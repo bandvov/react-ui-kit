22403e1871ff6770ecdc06621d396e7bee480164
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-05-29 14:15:51 +0300

    fix swiper logic

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,10 +1,4 @@
-import React, {
-  Dispatch,
-  SetStateAction,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
 import styled from "styled-components";
 import Tab from "./Tab";
 
@@ -24,40 +18,71 @@ export default function Tabs({
   value: string | number;
   onChange: Dispatch<SetStateAction<string>>;
 }) {
-  const [holding, setHolding] = useState(false);
-  const [tabOffset, setTabOffset] = useState(0);
-  const [prevTabOffset, setPrevTabOffset] = useState(0);
-  const tabsRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const childrenRef = useRef<HTMLDivElement>(null);
+  const startXRef = useRef(0);
+
+  const [offsetX, setOffsetX] = useState(0);
+
+  const onTouchStart = (e: any) => {
+    const clientX = e.clientX ? e.clientX : e.touches[0].clientX;
+
+    startXRef.current = offsetX + clientX;
+
+    window.addEventListener("mouseup", onTouchEnd);
+    window.addEventListener("touchend", onTouchEnd);
+    window.addEventListener("touchmove", onTouchMove);
+    window.addEventListener("mousemove", onTouchMove);
+  };
+
+  const onTouchMove = (e: any) => {
+    const currentX = e.clientX ? e.clientX : e.touches[0].clientX;
+
+    const diff = startXRef.current - currentX;
+
+    const currentOffsetLeft = childrenRef.current?.offsetLeft || 0;
+
+    let newOffset = currentOffsetLeft + diff;
+    if (newOffset < 0) {
+      newOffset = 0;
+    }
+    if (
+      newOffset >
+      childrenRef.current!.offsetWidth - containerRef.current!.offsetWidth
+    ) {
+      newOffset =
+        childrenRef.current!.offsetWidth - containerRef.current!.offsetWidth;
+    }
+    setOffsetX(newOffset);
+  };
+
+  const onTouchEnd = () => {
+    window.removeEventListener("touchmove", onTouchMove);
+    window.removeEventListener("mousemove", onTouchMove);
+    window.removeEventListener("mouseup", onTouchEnd);
+    window.removeEventListener("touchend", onTouchEnd);
+  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <div>
-        <div>{prevTabOffset}</div>
-        <div>{tabOffset}</div>
+        <div>{startXRef.current}</div>
+        <div>{offsetX}</div>
       </div>
       <StyledTabs
-        ref={tabsRef}
-        onMouseDown={(e) => {
-          setTabOffset(e.clientX);
-          setHolding(true);
-        }}
-        onMouseUp={(e) => {
-          setHolding(false);
-        }}
-        onMouseMove={(e) => {
-          if (tabsRef.current && holding) {
-            setPrevTabOffset(Math.floor(tabsRef.current.scrollLeft));
-            if (tabOffset > e.clientX) {
-              tabsRef.current.scrollLeft -= (e.clientX - tabOffset) / 20;
-            } else {
-              tabsRef.current.scrollLeft += (tabOffset - e.clientX) / 20;
-            }
-          }
-        }}
+        ref={containerRef}
+        onTouchStart={onTouchStart}
+        onMouseDown={onTouchStart}
+        // onMouseUp={(e) => {}}
+        // onTouchMove={(e: React.TouchEvent<HTMLDivElement>) => {}}
+        style={{ border: "3px solix" }}
+        onMouseLeave={onTouchEnd}
       >
         <div
+          ref={childrenRef}
           style={{
             display: "flex",
+            transform: `translateX(-${offsetX}px)`,
           }}
         >
           <Tab
